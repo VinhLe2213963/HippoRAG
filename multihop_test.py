1d86d0621ea6bf2986771c278ab3badc4507649e
VinhLe2213963 <167350988+VinhLe2213963@users.noreply.github.com>
2025-05-29 03:26:18 +0000

    Update multihop_test.py

--- multihop_test.py
+++ multihop_test.py
@@ -3,19 +3,14 @@ from typing import List
 import json
 import argparse
 import logging
-
+import pandas as pd
 from src.hipporag import HippoRAG
 
 def main():
-    docs_paths = ["sam1.txt", "sam2.txt", "trump1.txt", "trump2.txt", "yes1.txt", "yes2.txt"]
+    df = pd.read_csv("multihoprag_corpus_summary.csv")
 
     # Prepare datasets and evaluation
-    docs = []
-
-    for path in docs_paths:
-        with open(path, "r") as f:
-            content = f.read()
-            docs.append(content)
+    docs = list(df['content'])
 
     save_dir = 'outputs/openai'  # Define save directory for HippoRAG objects (each LLM/Embedding model combination will create a new subdirectory)
     llm_model_name = 'openai/gpt-4o'  # Any OpenAI model name
@@ -35,20 +30,29 @@ def main():
     queries = [
         "Who is the individual associated with the cryptocurrency industry facing a criminal trial on fraud and conspiracy charges, as reported by both The Verge and TechCrunch, and is accused by prosecutors of committing fraud for personal gain?",
         "Which individual is implicated in both inflating the value of a Manhattan apartment to a figure not yet achieved in New York City's real estate history, according to 'Fortune', and is also accused of adjusting this apartment's valuation to compensate for a loss in another asset's worth, as reported by 'The Age'?",
-        "Do the TechCrunch article on software companies and the Hacker News article on The Epoch Times both report an increase in revenue related to payment and subscription models, respectively?"
+        "Who is the figure associated with generative AI technology whose departure from OpenAI was considered shocking according to Fortune, and is also the subject of a prevailing theory suggesting a lack of full truthfulness with the board as reported by TechCrunch?"
     ]
 
     # For Evaluation
     answers = [
-        ["Sam Bankman-Fried"],
-        ["Donald Trump"],
-        ["Yes"]
+        "Sam Bankman-Fried",
+        "Donald Trump",
+        "Sam Altman"
     ]
 
     gold_docs = [
-        docs[0:2],
-        docs[2:4],
-        docs[4:]
+        [   
+            df.loc[df['title'] == "The FTX trial is bigger than Sam Bankman-Fried", 'content'].iloc[0],
+            df.loc[df['title'] == "SBF\u2019s trial starts soon, but how did he \u2014 and FTX \u2014 get here?", 'content'].iloc[0]
+        ],
+        [
+            df.loc[df['title'] == "Donald Trump defrauded banks with 'fantasy' to build his real estate empire, judge rules in a major repudiation against the former president", 'content'].iloc[0],
+            df.loc[df['title'] == "The $777 million surprise: Donald Trump is getting richer", 'content'].iloc[0]
+        ],
+        [
+            df.loc[df['title'] == "OpenAI's ex-chairman accuses board of going rogue in firing Altman: 'Sam and I are shocked and saddened by what the board did'", 'content'].iloc[0],
+            df.loc[df['title'] == "WTF is going on at OpenAI? We have theories", 'content'].iloc[0]
+        ]
     ]
 
     print(hipporag.rag_qa(queries=queries,
