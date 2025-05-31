570d639ae889d3484282ea3d614972798a74c9c6
VinhLe2213963 <167350988+VinhLe2213963@users.noreply.github.com>
2025-05-31 12:25:04 +0000

    Update multihop_test.py

--- src/hipporag/HippoRAG.py
+++ src/hipporag/HippoRAG.py
@@ -499,7 +499,9 @@ class HippoRAG:
             overall_qa_em_result, example_qa_em_results = qa_em_evaluator.calculate_metric_scores(
                 gold_answers=gold_answers, predicted_answers=[qa_result.answer for qa_result in queries_solutions],
                 aggregation_fn=np.max)
-            print(queries_solutions)
+            print("------------------------------------------------------------------")
+            print("QUERIES: ", queries_solutions)
+            print("------------------------------------------------------------------")
             overall_qa_f1_result, example_qa_f1_results = qa_f1_evaluator.calculate_metric_scores(
                 gold_answers=gold_answers, predicted_answers=[qa_result.answer for qa_result in queries_solutions],
                 aggregation_fn=np.max)
